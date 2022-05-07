af0dd9ea6199a5116d734110435508ba32fa01ed
Ezequiel Soares <mr.ezequiel.soares@gmail.com>
2022-05-06 22:58:18 -0500

    feat: :sparkles: toggle theme

--- src/components/templates/Header/index.tsx
+++ src/components/templates/Header/index.tsx
@@ -1,14 +1,35 @@
+import { Moon, Sun } from '../../icons'
 import Title from '../Title'
 
 interface HeaderProps {
   title: string
   subTitle: string
+  isDarkMode: boolean
+  setIsDarkMode: React.Dispatch<React.SetStateAction<boolean>>
 }
 
-const Header: React.FC<HeaderProps> = ({ title, subTitle }) => {
+const Header: React.FC<HeaderProps> = ({
+  title,
+  subTitle,
+  isDarkMode,
+  setIsDarkMode
+}) => {
   return (
     <header className="border-b-2 px-7 h-20 flex justify-between items-center dark:border-gray-800">
       <Title title={title} subTitle={subTitle} />
+
+      <div className="flex items-center gap-3">
+        <button
+          className="flex items-center justify-center h-10 w-10 bg-gray-200 dark:bg-gray-800 rounded-full text-gray-700 dark:text-gray-300 hover:bg-gray-300 hover:dark:bg-slate-700 hover:-translate-y-1 transition-all text-xs"
+          onClick={() => setIsDarkMode(prev => !prev)}
+        >
+          {isDarkMode ? <Moon /> : <Sun />}
+        </button>
+
+        <p className="text-gray-600 dark:text-gray-400">
+          {isDarkMode ? 'Dark Mode' : 'Light Mode'}
+        </p>
+      </div>
     </header>
   )
 }
--- src/components/templates/Layout/index.tsx
+++ src/components/templates/Layout/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from 'react'
 import Header from '../Header'
 import Main from '../Main'
 import SideMenu from '../SideMenu'
@@ -9,12 +10,35 @@ interface LayoutProps {
 }
 
 const Layout: React.FC<LayoutProps> = ({ title, subTitle, children }) => {
+  const [isDarkMode, setIsDarkMode] = useState(true)
+  const firstRender = useRef(true)
+
+  useEffect(() => {
+    const localStorageDarkMode = localStorage.getItem('darkMode')
+    if (localStorageDarkMode !== undefined && localStorageDarkMode !== null) {
+      setIsDarkMode(JSON.parse(localStorageDarkMode))
+    }
+  }, [])
+
+  useEffect(() => {
+    if (!firstRender.current) {
+      localStorage.setItem('darkMode', JSON.stringify(isDarkMode))
+    }
+
+    firstRender.current = false
+  }, [isDarkMode])
+
   return (
-    <div className="dark flex h-screen w-screen">
+    <div className={`flex h-screen w-screen ${isDarkMode && 'dark'}`}>
       <SideMenu />
 
       <div className="flex w-full flex-col bg-gray-100 dark:bg-slate-900">
-        <Header title={title} subTitle={subTitle} />
+        <Header
+          title={title}
+          subTitle={subTitle}
+          setIsDarkMode={setIsDarkMode}
+          isDarkMode={isDarkMode}
+        />
         <Main>{children}</Main>
       </div>
     </div>
